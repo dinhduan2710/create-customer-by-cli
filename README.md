170d9fd371de7fe0f5958920ad09025b580b4731
Fergus Dinh <fergus.dinh@gmail.com>
2023-03-02 11:13:29 +0700

    create

---
+++ README.md
@@ -0,0 +1,5 @@
+# CREATE CUSTOMER BY CLI
+
+``
+bin/magento customer:user:create --customer-firstname="duan" --customer-lastname="nguyen" --customer-email="dinhduan@gmail.com" --customer-password="admin@123" --website="1"
+``
---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Landofcoder
+ *
+ * @category    Landofcoder
+ * @package     Lof_AddressUpload
+ * @copyright   Copyright (c) Landofcoder (https://landofcoder.com/)
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Lof_Customer',
+    __DIR__
+);
